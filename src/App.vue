9cebb16f9ddd900bca79c2df35cdf876549dac2c
ZoeJi <zoe.q.ji@gmail.com>
2021-11-16 22:44:37 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ msg }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    }
   }
 }
 </script>
@@ -17,10 +21,5 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+
+  },
+  mutation: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+    
+  }
+})
